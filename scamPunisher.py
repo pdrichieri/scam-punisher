4765687e3ecdb6e8f3e6750c465c0fc49949255b
Pablo <pdrichieri@gmail.com>
2023-07-09 19:27:35 -0300

    first commit

---
+++ scamPunisher.py
@@ -0,0 +1,25 @@
+import requests
+import os
+import random
+import string
+import json
+
+emails = json.loads(open('emails.json').read())
+passwords = json.loads(open('passwords.json').read())
+
+url = 'https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
+
+for i in range(0, 3):
+    randEmailIndex = random.randint(0,499)
+    randPasswordIndex = random.randint(0,499)
+
+    print ('Sending user %s with password %s' % (emails[randEmailIndex], passwords[randPasswordIndex]))
+
+data = {
+    
+    }
+
+pato = requests.post(url, allow_redirects= False)
+
+print('Response: %s' % (pato.content))
+
